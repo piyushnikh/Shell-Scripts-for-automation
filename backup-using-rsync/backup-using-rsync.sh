bfb34eea0db5ae214a70a1286af81df76d4475b4
Piyush Nikhade <91729663+piyushnikh@users.noreply.github.com>
2024-07-15 10:37:56 +0530

    Update backup-using-rsync.sh

--- backup-using-rsync/backup-using-rsync.sh
+++ backup-using-rsync/backup-using-rsync.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
 
-rsync -av -e ssh <source directory from local server> <user_name>@<IP Address>:<destination directory to remote server>
+read -p "Enter the remote server username : " user_name
+
+read -p "Enter the remote server directory : " dir_name
+
+read -p "Enter the IP Address of remote machine : " ip
+
+read -p "Enter the path for which backup needs to be taken : " src_path
+
+if [ -z "$path" ]; then
+
+echo "Starting to back up the directory on remote server.................."
+
+rsync -av -e ssh "$src_path" "$user_name"@"$ip":"$dir_name"
+
+else
+
+echo "Input directory doesn't exist give a valid path !"
+
+exit 1
+
+fi
