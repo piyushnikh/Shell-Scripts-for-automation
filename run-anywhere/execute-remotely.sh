527bab1d136a0a9c32016bdcd61469448a58eba2
Piyush Nikhade <91729663+piyushnikh@users.noreply.github.com>
2024-07-18 13:01:40 +0530

    Create execute-remotely.sh

---
+++ run-anywhere/execute-remotely.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Array declaration the below IP addresses are not valid, just for demo purposes.
+
+servers=("13.201.135.240" "65.2.128.109" "15.206.70.149")
+
+# Make sure that script not executed with root
+
+if [ "$(id -u)" -eq 0 ]; then
+
+echo "Please run this script as a normal user" >&2
+
+exit 1
+
+fi
+
+# Initialize command variable
+
+COMMAND="$1"
+
+# Loop through SERVER_LIST
+
+for SERVER in "${servers[@]}"
+
+do
+
+ssh piyush@"$SERVER" "$COMMAND"
+
+sleep 3
+
+done
