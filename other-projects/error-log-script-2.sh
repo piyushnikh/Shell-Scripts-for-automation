d0cd8145997901283f4c1b39fad4d0d4bc3a0520
Piyush Nikhade <91729663+piyushnikh@users.noreply.github.com>
2024-07-25 10:56:11 +0530

    Delete other-projects directory

--- other-projects/error-log-script-2.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# The script will find print the unique file names & search word "error" or "warnings" in all the "*.log" files at /var/log directory.
-
-grep -iE "error|warnings" /var/log/*.log | awk -F ":" '{print $1}' | sort | uniq -c
