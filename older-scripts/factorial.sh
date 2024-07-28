2499f19337f13a2e9de1aabed3e37ae8d2f6f1db
pn02398 <nothing@gmail.com>
2024-07-28 15:26:27 +0530

    Changed factorial.sh

--- older-scripts/factorial.sh
+++ older-scripts/factorial.sh
@@ -1,20 +1,32 @@
 #!/bin/bash
 
+#Take input from user
+
+read -p "Enter your input number : " a
+
+#Initialize a variable
+
 b=1
 
-read -p "Enter your desired number : " a
+#Intialize a function to calculate factorial
 
-if [ "$a" -eq 0 ]; then
-	echo "The factorial of 0 is always 1"
+factorial () {
+if [ "$a" -lt 0 ]; then
+echo "Factorial of negative number is not defined, please enter a positive number"
+elif [ "$a" -eq 0 ]; then
+echo "Factorial of 0 is always 1"
 elif [ "$a" -eq 1 ]; then
-	echo "The factorial of 1 is always 1"
-elif [ "$a" -lt 0 ]; then
-	echo "Please enter positive number"
+echo "Factorial of 1 is always 1"
 else
-	while [ "$a" -gt 1 ]
-	do
-		b="$(echo ""$a" * "$b"" | bc -l)"
-		a="$(echo ""$a" - 1" | bc -l)"
-	done
-	echo "$b"
+while [ "$a" -gt 1 ];
+do
+b="$(echo ""$a" * "$b"" | bc -l)"
+a="$(echo ""$a" - 1" | bc -l)"
+done
 fi
+echo "$b"
+}
+
+#Function call
+
+factorial
